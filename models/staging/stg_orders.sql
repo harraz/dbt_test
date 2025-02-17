4dad741577577f0fd5bf2ed11154012a2d4e717c
@cmplrz <harraz@gmail.com>
2025-02-17 21:05:58 +0000

    added stage orders and profit by product report

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,26 @@
+select
+--from raw_orders
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+--o.customerid,
+--o.productid,
+ordersellingprice,
+ordercostprice,
+--from raw_customer
+c.customerid,
+c.customername,
+c.segment,
+c.country,
+--from raw_product
+p.productid,
+p.category,
+p.productname,
+p.subcategory,
+o.ordersellingprice - o.ordercostprice as orderprofit
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_product') }} as p
+on o.productid = p.productid
